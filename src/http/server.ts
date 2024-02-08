0bc89f432e5db41c8b955778d8a80f08177fcf6f
Maria Alice <alicedflopes.02@gmail.com>
2024-02-08 12:42:47 -0300

    feat: add base project
    initialize http server using fastify

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+import { fastify } from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+    return 'Hello, NLW!'
+})
+
+app.listen({port: 3333}, () => {
+    console.log('HTTP Server Running!')
+})
