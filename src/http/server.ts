d5488f5352d02d2352ce498be25d623c31a57aa8
Maria Alice <alicedflopes.02@gmail.com>
2024-02-09 23:44:37 -0300

    feat(api): add poll creation route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,38 @@
 import { fastify } from 'fastify'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+
+//conexão com o prisma
+const prisma = new PrismaClient()
 
 app.get('/hello', () => {
     return 'Hello, NLW!'
 })
 
-app.listen({port: 3333}, () => {
-    console.log('HTTP Server Running!')
+app.post('/polls', async(request, reply) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+    
+    const { title } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+        //dados que você quer inserir
+        data: {
+            title,
+        }
+    })
+
+    return reply 
+    .code(201)
+    .send({
+        message: 'Enquete criada com sucesso!',
+        pollId: poll.id 
+    })
+
 })
+
+app.listen({port: 3333}).then(() => {
+    console.log('HTTP Server Running!')})
